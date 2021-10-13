6ec5ca552417ed02541a6d2996daf5b4654e19e1
PillouMatou <vincent.armone@etu.univ-grenoble-alpes.fr>
2021-10-13 12:00:02 +0200

    download a picto work well

--- src/app/components/translate-picto/translate-picto.component.ts
+++ src/app/components/translate-picto/translate-picto.component.ts
@@ -12,7 +12,7 @@ export class TranslatePictoComponent implements OnInit {
 
   result:any;
   cellsToScroll:number = 4;
-
+  wordSearch:string = '';
 
 
   constructor() { }
@@ -23,6 +23,7 @@ export class TranslatePictoComponent implements OnInit {
 
   onSubmit(formText: NgForm) {
     this.resetResult();
+    this.wordSearch = formText.form.value.text;
     monitorInput(formText.form.value.text);
     this.result = getUrlPicto();
     console.log('le resultat en TS',this.result);
@@ -31,7 +32,32 @@ export class TranslatePictoComponent implements OnInit {
   resetResult(){
     this.result.length = 0;
   }
-  download(url: string){
-    console.log('image',url);
+  Download( url: any, filename: any ) {
+    let setFetching = false;
+    let setError = false;
+
+    const download = (url: RequestInfo, name: string | any[]) => {
+      if (!url) {
+        throw new Error("Resource URL not provided! You need to provide one");
+      }
+      setFetching =true;
+      fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+          setFetching =false;
+          const blobURL = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = blobURL;
+
+          if (name && name.length) if (typeof name === "string") {
+            a.download = name;
+          }
+          document.body.appendChild(a);
+          a.click();
+        })
+        .catch(() => setError = true);
+    };
+
+    download(url,filename);
   }
 }
